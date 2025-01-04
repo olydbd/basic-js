e559bf07a06bd25ed2c1fc8d863f558d6a292b6b
Olya Dubodel <dubodel.olya@gmail.com>
2025-01-04 23:14:16 +0300

    feat: solve the file-names task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,6 +16,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function repeater(/* str, options */) {
+  // let strCopy = str;
+  // if (options.repeatTimes) {
+  //   str = str.split(' ');
+  //   for (let i = 1; i < options.repeatTimes; i++) {
+  //     str.push(strCopy);
+  //   }
+
+  //   // Separator
+  //   if (options.separator) {
+  //     str = str.join(`${options.separator}`);
+  //   } else {
+  //     str = str.join('+');
+  //   }
+
+  //   // Addition
+  //   if (options.addition) {
+  //     let add = '' + options.addition;
+  //     let addCopy = add;
+
+  //     //Addition Repeat Times
+  //     if (options.additionRepeatTimes) {
+  //       add = add.split(' ');
+  //       for (let i = 1; i < options.additionRepeatTimes; i++) {
+  //         add.push(addCopy);
+  //       }
+
+  //       // Addition Separator
+  //       if (options.additionSeparator) {
+  //         add = add.join(`${options.additionSeparator}`);
+  //       } else {
+  //         add = add.join('|');
+  //       }
+  //     }
+
+  //     // Merge Str and Addition
+  //     if (options.separator) {
+  //       str = str.split(`${options.separator}`).map(item => item + add).join(`${options.separator}`);
+  //     } else {
+  //       str = str.split('+').map(item => item + add).join('+');
+  //     }
+  //   }
+  // }
+  // return str;
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let res = [];
+  let count = 1;
+  for (let i = 0; i < names.length; i++) {
+    if (!res.includes(names[i])) {
+      res.push(names[i]);
+    } else {
+      while (res.includes(names[i] + `(${count})`)) {
+        count++;
+      }
+      res.push(names[i] + `(${count})`);
+      count = 1;
+    }
+  }
+  return res;
 }
 
 module.exports = {
