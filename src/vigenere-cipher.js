012be3b3e8d235165f225c14b82851a97da2045a
Olya Dubodel <dubodel.olya@gmail.com>
2025-01-06 19:08:31 +0300

    feat: solve the vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,76 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(bool = true) {
+    this.bool = bool;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  formatKey(message, key) {
+    if (message.length !== key.length) {
+      key = key.repeat(Math.ceil(message.length / key.length));
+    }
+
+    let resKey = '';
+    let k = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] === ' ') {
+        resKey += ' ';
+        k++;
+        continue;
+      }
+      resKey += key[i - k];
+    }
+    return resKey;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let messageCopy = message.toUpperCase();
+    if (this.bool === false) {
+      messageCopy = messageCopy.split('').reverse().join('');
+    }
+    let keyCopy = this.formatKey(message, key).toUpperCase();
+    let res = '';
+
+    for (let i = 0; i < messageCopy.length; i++) {
+      if (messageCopy[i].codePointAt(0) < 65 || messageCopy[i].codePointAt(0) > 90) {
+        res += messageCopy[i];
+        continue;
+      }
+      let resCode = Math.abs((messageCopy[i].codePointAt(0) - 65 + keyCopy[i].codePointAt(0) - 65) % 26);
+      let resChar = String.fromCodePoint(resCode + 65);
+      res += resChar;
+    }
+
+    return res;
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let messageCopy = encryptedMessage.toUpperCase();
+    if (this.bool === false) {
+      messageCopy = messageCopy.split('').reverse().join('');
+    }
+    let keyCopy = this.formatKey(encryptedMessage, key).toUpperCase();
+    let res = '';
+
+    for (let i = 0; i < messageCopy.length; i++) {
+      if (messageCopy[i].codePointAt(0) < 65 || messageCopy[i].codePointAt(0) > 90) {
+        res += messageCopy[i];
+        continue;
+      }
+      let resCode = Math.abs((messageCopy[i].codePointAt(0) + 65 - keyCopy[i].codePointAt(0) + 65) % 26);
+      let resChar = String.fromCodePoint(resCode + 65);
+      res += resChar;
+    }
+
+    return res;
   }
 }
 
